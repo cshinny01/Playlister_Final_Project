e83cd55ffe5720bf2209454cbd2435d453887dad
cshinny01 <cshin01@hotmail.com>
2022-11-28 19:55:14 -0500

    Splash Screen almost done

--- client/src/components/HomeWrapper.js
+++ client/src/components/HomeWrapper.js
@@ -3,6 +3,7 @@ import HomeScreen from './HomeScreen'
 import SplashScreen from './SplashScreen'
 import AuthContext from '../auth'
 
+
 export default function HomeWrapper() {
     const { auth } = useContext(AuthContext);
     console.log("HomeWrapper auth.loggedIn: " + auth.loggedIn);
--- client/src/components/SplashScreen.js
+++ client/src/components/SplashScreen.js
@@ -1,7 +1,34 @@
+import { Link } from 'react-router-dom';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 export default function SplashScreen() {
     return (
         <div id="splash-screen">
             Playlister
+            <div id="playlister-description">
+                Create, find, and rate other people's playlists! With the only 
+                Playlist app you will ever need... Playlister!
+            </div>
+            <div id="have-account">
+                Have an account? Log in!
+            </div>
+            <Link to='/login'>
+                <Button
+                    variant="contained"
+                    size="medium"
+                    sx={{margin:2, top:5}}>Login</Button>
+            </Link>
+            <div id="dont-have-account">
+                Don't have an account? Register now!
+            </div>
+            <Link to='/register'>
+                <Button
+                    variant="contained"
+                    size="medium"
+                    sx={{margin:2, top:5}}>Register an Account!</Button>
+            </Link>
         </div>
     )
 }
